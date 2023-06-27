94c483469d12459bc898974a2cdccdb818642990
NicoDora <pjh76574734@gmail.com>
2023-06-27 14:03:54 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -13,5 +13,15 @@ function login() {
         id: id.value,
         password: password.value,
     };
+
     console.log(req);
+    console.log(JSON.stringify(req));
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Tpye": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
 }
